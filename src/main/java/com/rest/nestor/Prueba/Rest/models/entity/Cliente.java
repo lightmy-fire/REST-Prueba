fb3ffd30b29723fc593eec50cde4e8e67b04aee4
Néstor <nestor.amichetti@gmail.com>
2020-04-23 23:48:45 -0300

    probé el postman

--- src/main/java/com/rest/nestor/Prueba/Rest/models/entity/Cliente.java
+++ src/main/java/com/rest/nestor/Prueba/Rest/models/entity/Cliente.java
@@ -4,15 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
+@Table(name="clientes")
 public class Cliente implements Serializable{
 
 	private static final long serialVersionUID = 1L;
